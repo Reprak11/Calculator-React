eaf47cc3aedf2f6e5e00652a5a52008e615e0357
Reprak11 <reynaldo.armenta@cetys.edu.mx>
2020-12-15 01:18:12 -0800

    Avance 15/12/2020 Primera avance con TailwindCSS

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import {CalculatorProvider} from './hooks/calculatorContext'
 function App() {
   return (
     <CalculatorProvider>
-      <div>
-        <h1>Calculadora con React Hooks</h1>
-        <Display />
-        <ButtonPad />
-      </div>
+        <div className="bg-black h-screen flex flex-col items-center justify-center">
+          <h1 className="text-gray-50 pb-12 text-4xl font-semibold">Calculadora con React Hooks</h1>
+            <div className=" rounded-sm bg-gray-700 w-2/4 h-3/5">
+              <Display />
+              <ButtonPad />
+            </div>
+        </div>
     </CalculatorProvider>
   );
 }
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -3,9 +3,9 @@ import {useCalculator} from '../hooks/calculatorContext'
 
 const Display = () => {
     const displayValue = useCalculator()
-    return(<div>
-        <p>{displayValue.disValue}</p>
-        <p>{displayValue.disNumber}</p>
+    return(<div className="pt-5">
+        <p className="truncate text-red-700 font-semibold text-2xl">{displayValue.disValue}</p>
+        <p className="truncate text-gray-50 text-4xl">{displayValue.disNumber}</p>
     </div>)
 }
 
